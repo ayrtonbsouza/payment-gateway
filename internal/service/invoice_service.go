f934b5fc6ecde0e6c1c01bce1b1e35d8a2dd8e7f
Ayrton Souza <ayrtonsouza@icloud.com>
2025-04-13 18:24:37 -0300

    [:construction:] creating invoice creation method on invoice service

--- internal/service/invoice_service.go
+++ internal/service/invoice_service.go
@@ -1,6 +1,9 @@
 package service
 
-import "github.com/ayrtonbsouza/payment-gateway/internal/domain"
+import (
+	"github.com/ayrtonbsouza/payment-gateway/internal/domain"
+	"github.com/ayrtonbsouza/payment-gateway/internal/dto"
+)
 
 type InvoiceService struct {
 	invoiceRepository domain.InvoiceRepository
@@ -12,4 +15,34 @@ func NewInvoiceService(invoiceRepository domain.InvoiceRepository, accountServic
 		invoiceRepository: invoiceRepository,
 		accountService:    accountService,
 	}
+}
+
+func (s *InvoiceService) CreateInvoice(input *dto.CreateInvoiceinput) (*dto.InvoiceOutput, error) {
+	accountOutput, err := s.accountService.FindByAPIKey(input.APIKey)
+	if err != nil {
+		return nil, err
+	}
+
+	invoice, err := dto.ToInvoice(*input, accountOutput.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := invoice.Process(); err != nil {
+		return nil, err
+	}
+
+	if invoice.Status == domain.StatusApproved {
+		_, err = s.accountService.UpdateBalance(input.APIKey, invoice.Amount)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if err := s.invoiceRepository.Save(invoice); err != nil {
+		return nil, err
+	}
+
+	output := dto.FromInvoice(invoice)
+	return &output, nil
 }
